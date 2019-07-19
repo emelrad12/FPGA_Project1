ae51838b811498935316bd008376c743aba68375
emelrad12 <emelrad12@gmail.com>
2019-07-19 22:13:46 +0200

    Style: Changed names

--- twoBitMultiplier.v
+++ twoBitMultiplier.v
@@ -2,25 +2,25 @@ module twoBitMultiplier (A0, A1, B0, B1, S0, S1, S2, S3, IZero, clk);
 
 input wire A0, A1, B0, B1, IZero, clk;
 output wire S0, S1, S2, S3;
-wire A1tmp, A0tmp, B0tmp;
-reg A1tmpr, A0tmpr, B0tmpr, S0r;
+wire A1t, A0t, B0t;
+reg A1tr, A0tr, B0tr, S0r;
 
-assign A1tmp = A1tmpr;
-assign A0tmp = A0tmpr;
-assign B0tmp = B0tmpr;
+assign A1t = A1tr;
+assign A0t = A0tr;
+assign B0t = B0tr;
 assign S0 = S0r;
 always @(posedge clk) begin
 
-A1tmpr = B1 & A1;
-A0tmpr = B1 & A0;
-B0tmpr = B0 & A1;
+A1tr = B1 & A1;
+A0tr = B1 & A0;
+B0tr = B0 & A1;
 S0r = B0 & A0;
 
 end
 twoBitFullAdder twoBitAdd(
-		.A0	(A0tmp),
-		.A1	(A1tmp),
-		.B0	(B0tmp),
+		.A0	(A0t),
+		.A1	(A1t),
+		.B0	(B0t),
 		.B1	(IZero),
 		.Ci   (IZero),
 		.Co	(S3),
