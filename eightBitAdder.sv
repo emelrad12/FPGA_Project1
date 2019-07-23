06124c435650a433e91ebc1e0487d062827914c4
emelrad12 <emelrad12@gmail.com>
2019-07-23 23:53:47 +0200

    no message

--- eightBitAdder.sv
+++ eightBitAdder.sv
@@ -1,20 +1,17 @@
-module eightBitAdder (A,B,Ci,Co,S);
+module eightBitAdder (A,B,Ci,Co,S,sas);
 
 input wire [0:7]A;
 input wire [0:7]B;
 input wire Ci;
 output wire Co;
 output wire [0:7]S;
+output wire sas;
 wire Tpmw;
-wire arrayA1,arrayA2,arrayB1,arrayB2,arratS1,arrayS2;
+wire [0:3]arrayA1,arrayA2,arrayB1,arrayB2,arrayS1,arrayS2;
 assign arrayA1=A[0:3];
 assign arrayA2=A[4:7];
 assign arrayB1=B[0:3];
 assign arrayB2=B[4:7];
-assign arratS1=S[0:3];
-assign arrayS2=S[4:7];
-
-
 fourBitFullAdder Adder1(
    .A    (arrayA1),
 	.B    (arrayB1),
@@ -25,9 +22,10 @@ fourBitFullAdder Adder1(
 fourBitFullAdder Adder2(
    .A    (arrayA2),
 	.B    (arrayB2),
-	.Ci   (Tmpw),
+	.Ci   (Tpmw),
 	.Co   (Co),
 	.S    (arrayS2)
 	);
-	
+assign S={arrayS1,arrayS2};
+
 endmodule
