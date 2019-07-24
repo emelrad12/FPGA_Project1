580375225916d94b0c0a80c11c681ddb703f1ea1
emelrad12 <emelrad12@gmail.com>
2019-07-24 21:31:54 +0200

    no message

--- sixteenBitAdder.sv
+++ sixteenBitAdder.sv
@@ -1,6 +1,6 @@
-module sixteenBitAdder (A, B, Ci, Co, S, split);
+module sixteenBitAdder (A, B, Ci, Co, S, split16);
 input wire [0:15]A, B;
-input wire Ci, split;
+input wire Ci, split16;
 output wire [0:15]S;
 output wire Co;
 wire [0:7]inputOneFirstHalf, inputOneSecondHalf, inputTwoFirstHalf, inputTwoSecondHalf, OutputFirstHalf, OutputSecondHalf;
@@ -11,8 +11,8 @@ assign inputOneSecondHalf=A[8:15];
 assign inputTwoFirstHalf=B[0:7];
 assign inputTwoSecondHalf=B[8:15];
 eightBitAdder Adder1(inputOneFirstHalf, inputTwoFirstHalf, Ci, Ctr, OutputFirstHalf);
-assign Ctr2= (split==1)?Ctr:0;
-//condInput splitter(Ctr, split, Ctr2);
+assign Ctr2= (split16==1)?0:Ctr;
+//condInput splitter(Ctr, split16, Ctr2);
 eightBitAdder Adder2(inputOneSecondHalf, inputTwoSecondHalf, Ctr2, Co, OutputSecondHalf);
 assign S={OutputFirstHalf,OutputSecondHalf};
 endmodule
