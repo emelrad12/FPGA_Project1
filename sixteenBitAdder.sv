1a647c5ea3352d3c8a7b3ddf0ffa7d0b6d22e89d
emelrad12 <emelrad12@gmail.com>
2019-07-24 21:09:32 +0200

    no message

--- sixteenBitAdder.sv
+++ sixteenBitAdder.sv
@@ -1,14 +1,18 @@
 module sixteenBitAdder (A, B, Ci, Co, S, split);
-input wire [0:7]A, B;
+input wire [0:15]A, B;
 input wire Ci, split;
 output wire [0:15]S;
 output wire Co;
-wire inputFirstHalf,inputSecondHalf;
-wire OutputFirstHalf, OutputSecondHalf;
-reg carrytr;
-assign inputFirstHalf=A[0:7];
-assign inputSecondHalf=A[8:15];
-assign OutputFirstHalf=B[0:7];
-assign OutputSecondHalf=B[8:15];
-assign S={inputFirstHalf,inputSecondHalf};
+wire [0:7]inputOneFirstHalf, inputOneSecondHalf, inputTwoFirstHalf, inputTwoSecondHalf, OutputFirstHalf, OutputSecondHalf;
+wire Ctr;
+wire Ctr2;
+assign inputOneFirstHalf=A[0:7];
+assign inputOneSecondHalf=A[8:15];
+assign inputTwoFirstHalf=B[0:7];
+assign inputTwoSecondHalf=B[8:15];
+eightBitAdder Adder1(inputOneFirstHalf, inputTwoFirstHalf, Ci, Ctr, OutputFirstHalf);
+//assign Ctr2= (split==1)?Ctr:0;
+condInput splitter(Ctr, split, Ctr2);
+eightBitAdder Adder2(inputOneSecondHalf, inputTwoSecondHalf, Ctr2, Co, OutputSecondHalf);
+assign S={OutputFirstHalf,OutputSecondHalf};
 endmodule
